1eeda161ab2b3ab8e191117fe15c1ddd365db6cf
AttakDoge <liambragg1@gmail.com>
2023-07-12 15:58:40 -0400

    batch file updates
    removed the v1 file, its not going to be focused on
    
    testing file updated - i will be trying to do my best to make this as stable as possible

--- better-rblx-testing.cmd
+++ better-rblx-testing.cmd
@@ -1,7 +1,30 @@
-@echo off
+:: TO DO: CLEAN UP CODE
+
+::@echo off
 title Better-RBX
 
-for /F "tokens=*" %%a in ('where /r C:\Users\liamb\AppData\Local\Roblox RobloxPlayerBeta.exe') do set output=%%a
+for /F "tokens=*" %%a in ('where /r C:\Users\liamb\AppData\Local\Roblox RobloxPlayerBeta.exe') do set rbxFolder=%%a
 ::where /r C:\Users\liamb\AppData\Local\Roblox RobloxPlayerBeta.exe
+set rbxFolder=%rbxFolder:RobloxPlayerBeta.exe=%
+echo %rbxFolder%
+::outputs file path to parent folder of robloxplayerbeta.exe
+
+for %%i in ("%~dp0.") do set "parentFolder=%%~fi"
+
+
+::go to version path
+C:
+cd %rbxFolder%
+::create client settings
+mkdir ClientSettings
+copy /v/y %parentFolder%\ClientAppSettings.json %rbxFolder%ClientSettings\
+
+goto comment
+::topbar replacement
+copy /v/y %parentFolder%\coloredlogo.png  %rbxFolder%content\textures\ui\TopBar\
+copy /v/y %parentFolder%\coloredlogo@2x.png  %rbxFolder%content\textures\ui\TopBar\
+copy /v/y %parentFolder%\coloredlogo@3x.png  %rbxFolder%content\textures\ui\TopBar\
+
+:comment
 
 pause
--- better-rblx-v1.cmd
+++
@@ -1,6 +0,0 @@
-::be sure to set the variable below to the path of the ClientAppSettings.json you downloaded
-::also be sure to have this cmd file in the folder that contains the file named RobloxPlayerBeta.exe, PREFERABLY COPIED
-::this file path should be if roblox was installed normally: C:\Users\(your user profile name)\AppData\Local\Roblox\Versions\(roblox game version)
-
-set clientsettingspath=D:\vsc-projects\roblox-but-better\ClientAppSettings.json
-
